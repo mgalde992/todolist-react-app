f82502a1ce014496895094be8bfd54c3466ee247
Mohamed Galde <mgalde992@hotmail.com>
2020-04-25 22:12:54 +0300

    Updates

---
+++ precache-manifest.8fd195cf8f0959a6409671ef63a19ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61575a849a26adf6107970ff700311a",
+    "url": "/mgalde992/todolist-react-app.git/index.html"
+  },
+  {
+    "revision": "53bf3a4b6d2333a9d457",
+    "url": "/mgalde992/todolist-react-app.git/static/css/main.a7e224c4.chunk.css"
+  },
+  {
+    "revision": "f9d3a3e7c2811db8ad45",
+    "url": "/mgalde992/todolist-react-app.git/static/js/2.8f7c539e.chunk.js"
+  },
+  {
+    "revision": "53bf3a4b6d2333a9d457",
+    "url": "/mgalde992/todolist-react-app.git/static/js/main.03e3fe0e.chunk.js"
+  },
+  {
+    "revision": "72deb5af2b1ebe2c0a79",
+    "url": "/mgalde992/todolist-react-app.git/static/js/runtime~main.b67496dd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mgalde992/todolist-react-app.git/static/media/logo.ee7cd8ed.svg"
+  }
+]);
